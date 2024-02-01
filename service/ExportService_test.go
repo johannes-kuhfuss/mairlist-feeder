b009ca3384cfbb47ca565bf421fc392ef57f5171
johannes-kuhfuss <johannes.kuhfuss@web.de>
2024-02-01 08:25:33 +0100

    refactoring, testing

--- service/ExportService_test.go
+++ service/ExportService_test.go
@@ -1,10 +1,12 @@
 package service
 
 import (
+	"io"
 	"testing"
 
 	"github.com/johannes-kuhfuss/mairlist-feeder/config"
 	"github.com/johannes-kuhfuss/mairlist-feeder/repositories"
+	"github.com/stretchr/testify/assert"
 )
 
 var (
@@ -19,4 +21,34 @@ func setupTest(t *testing.T) func() {
 	exportService = NewExportService(&cfg, &fileRepo)
 	return func() {
 	}
+}
+
+func Test_buildHttpRequest_EmptyUrl_ReturnsError(t *testing.T) {
+	tearDown := setupTest(t)
+	defer tearDown()
+	cfg.Export.MairListUrl = ""
+
+	req, err := exportService.buildHttpRequest("test")
+
+	assert.Nil(t, req)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, "url cannot be empty", err.Error())
+}
+
+func Test_buildHttpRequest_WithUrl_ReturnsRequest(t *testing.T) {
+	tearDown := setupTest(t)
+	defer tearDown()
+	cfg.Export.MairListUrl = "http://localhost:9300/"
+	cfg.Export.MairListUser = "test"
+	cfg.Export.MairListPassword = "test"
+
+	req, err := exportService.buildHttpRequest("test")
+
+	assert.NotNil(t, req)
+	assert.Nil(t, err)
+	assert.EqualValues(t, "http://localhost:9300/execute", req.URL.String())
+	assert.EqualValues(t, "application/x-www-form-urlencoded", req.Header.Get("Content-Type"))
+	assert.EqualValues(t, "Basic dGVzdDp0ZXN0", req.Header.Get("Authorization"))
+	b, _ := io.ReadAll(req.Body)
+	assert.EqualValues(t, "command=PLAYLIST+1+APPEND+test", string(b))
 }
