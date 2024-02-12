1549bb85ae09d032f4adc1c33994bd45bc293f8b
johannes-kuhfuss <johannes.kuhfuss@web.de>
2024-02-12 08:04:16 +0100

    Unit tests

---
+++ dto/File_test.go
@@ -0,0 +1,72 @@
+package dto
+
+import (
+	"testing"
+	"time"
+
+	"github.com/johannes-kuhfuss/mairlist-feeder/config"
+	"github.com/johannes-kuhfuss/mairlist-feeder/domain"
+	"github.com/johannes-kuhfuss/mairlist-feeder/repositories"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	repo repositories.DefaultFileRepository
+	cfg  config.AppConfig
+)
+
+func setupTest(t *testing.T) func() {
+	repo = repositories.NewFileRepository(&cfg)
+	return func() {
+	}
+}
+
+func Test_GetFiles_NoFiles_ReturnsEmpty(t *testing.T) {
+	teardown := setupTest(t)
+	defer teardown()
+	res := GetFiles(&repo)
+	assert.EqualValues(t, 0, len(res))
+}
+
+func Test_GetFiles_TwoFiles_ReturnsFileData(t *testing.T) {
+	teardown := setupTest(t)
+	defer teardown()
+	fi1 := domain.FileInfo{
+		Path:                "A",
+		ModTime:             time.Time{},
+		Duration:            3600,
+		StartTime:           "11:00",
+		EndTime:             "",
+		FromCalCMS:          false,
+		InfoExtracted:       false,
+		ScanTime:            time.Time{},
+		FolderDate:          "2023-12-31",
+		RuleMatched:         "None",
+		EventId:             1,
+		CalCmsTitle:         "",
+		CalCmsInfoExtracted: false,
+	}
+	fi2 := domain.FileInfo{
+		Path:                "B",
+		ModTime:             time.Time{},
+		Duration:            3600,
+		StartTime:           "",
+		EndTime:             "13:00",
+		FromCalCMS:          false,
+		InfoExtracted:       false,
+		ScanTime:            time.Time{},
+		FolderDate:          "2023-12-31",
+		RuleMatched:         "None",
+		EventId:             1,
+		CalCmsTitle:         "",
+		CalCmsInfoExtracted: false,
+	}
+	repo.Store(fi1)
+	repo.Store(fi2)
+
+	res := GetFiles(&repo)
+
+	assert.EqualValues(t, 2, len(res))
+	assert.EqualValues(t, "2023-12-31", res[0].FolderDate)
+	assert.EqualValues(t, "2023-12-31", res[1].FolderDate)
+}
