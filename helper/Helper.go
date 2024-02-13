a2d03fcbb4efbb5ad57da37be9bf55a62c9a7843
johannes-kuhfuss <johannes.kuhfuss@web.de>
2024-02-13 08:19:57 +0100

    Time conversion

--- helper/Helper.go
+++ helper/Helper.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"errors"
 	"fmt"
 	"path"
 	"time"
@@ -16,4 +17,12 @@ func GetTodayFolder(test bool, testDate string) string {
 	day := fmt.Sprintf("%02d", time.Now().Day())
 
 	return path.Join(year, month, day)
+}
+
+func TimeFromHourAndMinute(hour int, minute int) (*time.Time, error) {
+	if (hour < 0) || (hour > 23) || (minute < 0) || (minute > 59) {
+		return nil, errors.New("hour must be between 0 and 23, minute must be between 0 and 59")
+	}
+	t := time.Date(1, 1, 1, hour, minute, 0, 0, time.Local)
+	return &t, nil
 }
