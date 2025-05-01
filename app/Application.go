97ec1d59a5e9318981a1a236d53d14d1879f4789
johannes-kuhfuss <johannes.kuhfuss@web.de>
2025-05-01 09:10:00 +0200

    removed insecure tls algo

--- app/Application.go
+++ app/Application.go
@@ -117,7 +117,6 @@ func initServer() {
 				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 			},
 			PreferServerCipherSuites: true,
 			MinVersion:               tls.VersionTLS12,
